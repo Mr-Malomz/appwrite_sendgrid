ffce9bed4c2e373faf32841a4b95c1722f958f23
demola malomo <malomo.alaba@yahoo.com>
2022-10-21 00:32:55 +0100

    code clean up

--- functions/appwrite_sendgrid/lib/main.dart
+++ functions/appwrite_sendgrid/lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:dart_appwrite/dart_appwrite.dart';
 import 'package:sendgrid_mailer/sendgrid_mailer.dart';
 
-/*
-  'req' variable has:
-    'headers' - object with request headers
-    'payload' - request body data as a string
-    'variables' - object with function variables
-
-  'res' variable has:
-    'send(text, status: status)' - function to return text response. Status code defaults to 200
-    'json(obj, status: status)' - function to return JSON response. Status code defaults to 200
-  
-  If an error is thrown, a response with code 500 will be returned.
-*/
-
 Future<void> start(final req, final res) async {
   Client client = Client();
 
